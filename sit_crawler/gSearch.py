4e42406951b6d6bdc6d090234ab2448c98b8f080
kxnjo <lauxinhui5@gmail.com>
2023-09-25 19:45:35 +0800

    gSearch | push module search function

--- sit_crawler/gSearch.py
+++ sit_crawler/gSearch.py
@@ -5,7 +5,7 @@ import pandas as pd
 API_KEY = 'AIzaSyDH6gfB_UlGeEtqXt7ZiHK7TdYydSTQWac'
 SEARCH_ENGINE_ID = '718bdbf89bb93481f'
 
-def build_payload(query, date_restrict = 'm1', **params):
+def build_payload(query, **params):
     """
     :param query = Search term
     :date_restrict = Restricts results based on recency (default is 1 month, 'm1')
@@ -16,20 +16,20 @@ def build_payload(query, date_restrict = 'm1', **params):
     payload = {
         'key': API_KEY,
         'q': query,
-        'cx': SEARCH_ENGINE_ID, 
-        'date_restrict': date_restrict
+        'cx': SEARCH_ENGINE_ID
     }
 
     payload.update(params)
     return payload
 
-def make_request(payload):
+def make_request(search, school):
     """
     function makes request to Google Search API and handle potential errors.
 
     :payload = dictionary containing request parameters
     :return = JSON response from Google Search API
     """
+    payload = build_payload(f'{search} {school}')
 
     response = requests.get("https://www.googleapis.com/customsearch/v1", params = payload)
     # check if there is any errors
@@ -38,16 +38,16 @@ def make_request(payload):
 
     return response.json()
 
-def find_module(search, school = "SIT"):
+def find_course(courseName, school = "Singapore Institute of Technology"):
     """
-    function searches and URL for result that's of most relevance to use case (SIT students)
+    function searches and URL for COURSE result that's of most relevance to use case (SIT students)
 
     :response = search listing object in JSON
     :return = URL for specified search
     """
     
-    payload = build_payload(f'{search} {school}') # build the payload
-    response = make_request(payload) # get list of google search responses in JSON
+     # build the payload
+    response = make_request(courseName, school) # get list of google search responses in JSON
 
     # check if the response is working well
     if response["items"]:
@@ -58,12 +58,41 @@ def find_module(search, school = "SIT"):
     # filter for school search (make sure that it is the right school)
     for item in itemsResponse:
         resultList = [item["title"], item["displayLink"], item["snippet"]]
+        print("\n\n this is result list")
+        print(resultList)
         for x in resultList:
-            if school in x:
+            # find school name (likely that link is related to the school)
+            if school in x or courseName in x:
                 return item["link"]
+
     
     return "link not found"
 
+def find_module(moduleName, school = "Singapore Institute of Technology"):
+    """
+    function searches and URL for MODULE result that's of most relevance to use case (SIT students)
+
+    :response = search listing object in JSON
+    :return = URL for specified search
+    """
+    moduleURL = "https://www.singaporetech.edu.sg/modules/"
+
+     # build the payload
+    response = make_request(f'{moduleName} module', school) # get list of google search responses in JSON
+
+    # check if the response is working well
+    if response["items"]:
+        itemsResponse = response["items"].copy()
+    else:
+        return "search not found"
+
+    # filter for school search (make sure that it is the right school)
+    for item in itemsResponse:
+        if moduleURL in item["link"]:
+            return item["link"]
+    return "module link not found"
+
 
 # example search
-print(find_module("software engineering"))
+# print(find_course("software engineering"))
+print(find_module("programming fundamentals"))
