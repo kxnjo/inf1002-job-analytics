effebf7f1db26809de30ec61500a22a8cbf7b1ba
crys <crystal.pmh@outlook.com>
2023-09-28 01:24:09 +0800

    comments and minor changes

--- sit_crawler/get_module_description.py
+++ sit_crawler/get_module_description.py
@@ -3,64 +3,63 @@ import requests
 import csv
 import pandas as pd
 
+#TODO: change this hardcoded value to use all data files in certain filepath instead
 filepaths = ["Information and Communications Technology (Information Security).csv", "Information and Communications Technology (Software Engineering).csv"]
 
 def main():
     for filepath in filepaths:
         module_title = []
         module_desc = []
+
         with open(f"data/{filepath}", encoding="utf-8") as f:
             reader = csv.reader(f)
-            next(reader)
+            next(reader) # skip header line
             for row in reader:
-                if row[1]:
+                if row[1]: # if link exists
                     title, desc = get_description(row[1])
                     module_title.append(title)
                     module_desc.append(desc)
-                    """
-                    if title == row[1]:
-                        module_desc.append(desc)
-                    else:
-                        module_desc.append("incorrect module")
-                    """
                 else:
                     module_title.append(None)
                     module_desc.append(None)
 
+        # Add module Title and Description extracted from the module page
         data = pd.read_csv(f"data/{filepath}")
         data["Title"] = module_title
         data["Description"] = module_desc
         data.to_csv(f"data/{filepath}", index=False)
 
+
 def get_description(url):
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
 
-    print(url)
+    # print(url) # for debug
 
     module = soup.find("div", class_="group-left")
 
     # Search for module description
     try:
-        module_desc = module.findChild("p")
-        # Exception cases that are styled differently on SIT website
-        if not module_desc:
-            module_desc = module.find("div", class_="row--top-md")
+        desc_element = module.findChild("p")
 
-        module_desc = module_desc.getText().strip()
+        # Exception cases that are styled differently on SIT website
+        if not desc_element:
+            desc_element = module.find("div", class_="row--top-md")
+
+        desc = desc_element.getText().strip()
 
     except Exception as e:
         print(f"Error getting description from {url}, {e}")
-        module_desc = None
+        desc = None
     
     # Search for module title
     try:
-        module_title = module.findChild("h1").getText().strip()
+        title = module.findChild("h1").getText().strip()
 
     except Exception as e:
         print(f"Error getting title from {url}, {e}")
-        module_title = None
+        title = None
 
-    return [module_title, module_desc]
+    return [title, desc]
 
 main()
