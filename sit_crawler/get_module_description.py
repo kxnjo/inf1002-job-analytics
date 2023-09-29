d15fd6212565b007bb8001a40c719debb9f010c0
crys <crystal.pmh@outlook.com>
2023-09-29 13:14:49 +0800

    Updated cleaner list of modules

--- sit_crawler/get_module_description.py
+++ sit_crawler/get_module_description.py
@@ -17,11 +17,15 @@ def main():
             for row in reader:
                 if row[1]: # if link exists
                     title, desc = get_description(row[1])
-                    module_title.append(title)
-                    module_desc.append(desc)
-                else:
-                    module_title.append(None)
-                    module_desc.append(None)
+                    # If module name of most relevant search matches the course page's module:
+                    if module_match(row[0], title):
+                        module_title.append(title)
+                        module_desc.append(desc)
+                        continue
+
+                # If link doesn't exist or if module name doesn't match, set value to None
+                module_title.append(None)
+                module_desc.append(None)
 
         # Add module Title and Description extracted from the module page
         data = pd.read_csv(f"data/{filepath}")
@@ -34,18 +38,15 @@ def get_description(url):
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
 
-    # print(url) # for debug
-
+    # Look for module in description page
     module = soup.find("div", class_="group-left")
 
     # Search for module description
     try:
         desc_element = module.findChild("p")
-
         # Exception cases that are styled differently on SIT website
         if not desc_element:
             desc_element = module.find("div", class_="row--top-md")
-
         desc = desc_element.getText().strip()
 
     except Exception as e:
@@ -55,11 +56,19 @@ def get_description(url):
     # Search for module title
     try:
         title = module.findChild("h1").getText().strip()
-
     except Exception as e:
         print(f"Error getting title from {url}, {e}")
         title = None
 
     return [title, desc]
+
+def module_match(mod_name, description_page_name):
+    clean_mod_name1 = mod_name.replace(" ", "").replace("-", "").replace("Organization", "Organisation")
+    clean_mod_name2 = description_page_name.replace(" ", "").replace("-", "").replace("Organization", "Organisation")
+
+    if clean_mod_name1.lower() == clean_mod_name2.lower():
+        return True
+    return False
+
 
 main()
